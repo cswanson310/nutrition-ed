3da262e9f4f9fa2dc6807dc5f761a624f0960316
Charlie Swanson <cswanson310@gmail.com>
2014-04-28 12:25:41 -0400

    fix data loading to have correct answer

--- db/fixtures/questions_and_answers.rb
+++ db/fixtures/questions_and_answers.rb
@@ -18,12 +18,13 @@ CSV.foreach("#{Rails.root}/db/fixtures/Questions.csv") do |row|
       s.prompt = row.first
       s.difficulty = row.second.to_i
       s.hint = row.third
+      s.correct_answer_id = (id-1)*4 + 1
     end
     Answer.seed(:id,
-      {id: id*4 + 1, question_id: id, answer_text: row[3]},
-      {id: id*4 + 2, question_id: id, answer_text: row[4], hint: row[5]},
-      {id: id*4 + 3, question_id: id, answer_text: row[6], hint: row[7]},
-      {id: id*4 + 4, question_id: id, answer_text: row[8], hint: row[9]},
+      {id: (id-1)*4 + 1, question_id: id, answer_text: row[3]},
+      {id: (id-1)*4 + 2, question_id: id, answer_text: row[4], hint: row[5]},
+      {id: (id-1)*4 + 3, question_id: id, answer_text: row[6], hint: row[7]},
+      {id: (id-1)*4 + 4, question_id: id, answer_text: row[8], hint: row[9]},
     )
     if (!skills.include?(row[10])) then
       Skill.seed do |s|
