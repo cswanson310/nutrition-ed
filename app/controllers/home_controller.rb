a4e6f23a0d30f600165c1334f764d63b7e6ee77b
Charlie Swanson <cswanson310@gmail.com>
2014-05-09 06:13:06 -0400

    Made it less likely to get repeat questions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,12 @@ class HomeController < ApplicationController
   def index
     # do nothing, just route to the page
   end
+
+  def kids
+
+  end
+
+  def parents
+
+  end
 end
--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -4,11 +4,11 @@ module QuestionHelper
     my_skills = current_user.user_skills.map{|us| [us.level, us.skill_id]} 
     worst_skill_level, worst_skill_id = my_skills.min
     possible_questions = Skill.find(worst_skill_id).questions
-    next_q = possible_questions.bsearch{|q| q.difficulty >= worst_skill_level}
+    next_q = possible_questions.select{|q| q.difficulty >= worst_skill_level - 1}.sample
     if next_q then
       return question_path(next_q)
     else
-      return question_path(possible_questions.select{|q| q.difficulty < worst_skill_level}.shuffle.first)
+      return question_path(possible_questions.select{|q| q.difficulty < worst_skill_level}.sample)
     end
   end
 
